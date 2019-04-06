1250e65b2daf0c736be233a6f7d98bc65152d18d
twmsllc <49331357+twmsllc@users.noreply.github.com>
2019-04-06 01:06:55 -0400

    Edit Descriptor
    Removed descriptor to place in ReadMe

--- xtimerindicator.py
+++ xtimerindicator.py
@@ -2,10 +2,6 @@
 # -*- coding: utf-8 -*-
 
 """
-    xTimerIndicator is a Gtk Application that adds a set of timers to the 
-    indicator section of the Gnome topbar using AppIndicator3
-    Command line functionality is included to allow the binding of Hotkeys to the
-    start/stop and reset functions.
 
     #######################################################
 
